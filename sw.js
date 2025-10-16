4b8db38298b5905b04a0e10db3d1e881dfc7fafc
Moxese <mosesemugera@gmail.com>
2025-10-16 09:06:48 +0300

    tt

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-const CACHE_NAME = 'workfit-v1.0';
+const CACHE_NAME = 'workfit-v1.2';
 const urlsToCache = [
-  '/workfit-wpa.html',
-  '/css/main.css',
-  '/js/app.js'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://i.postimg.cc/9fbXQC1m/Whats-App-Image-2025-10-04-at-06-52-57-removebg-preview-1.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -18,8 +21,31 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
-      }
-    )
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).catch(() => {
+          // If both cache and network fail, serve the offline page
+          if (event.request.mode === 'navigate') {
+            return caches.match('/index.html');
+          }
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
